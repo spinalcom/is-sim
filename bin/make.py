bf7aae2eccc0dab18afa07f2038fb75d3e13e49b
Clement Pianon <c.pianon@structure-computation.com>
2016-06-06 18:18:41 +0200

    added html files copy to browser-organ

--- bin/make.py
+++ bin/make.py
@@ -7,23 +7,17 @@ stylesheets = []
 
 for p in os.listdir( "modules" ):
     for m in os.listdir("modules/" + p):
-	if m == "models":
-	    models.append("gen/" + m + "/" + p + ".js")
+        if m == "models":
+            models.append("gen/" + m + "/" + p + ".js")
             concat_js( "modules/" + p + "/" + m, "gen/" + m + "/" + p + ".js", "gen/stylesheets/" + p + ".css" )
 
-	elif m == "views":
-	    views.append("gen/" + m + "/" + p + ".js")
+        elif m == "views":
+            views.append("gen/" + m + "/" + p + ".js")
             concat_js( "modules/" + p + "/" + m, "gen/" + m + "/" + p + ".js", "gen/stylesheets/" + p + ".css" )
 
-	elif m == "stylesheets":
-	    stylesheets.append("gen/" + m + "/" + p + ".css")
+        elif m == "stylesheets":
+            stylesheets.append("gen/" + m + "/" + p + ".css")
             concat_js( "modules/" + p + "/" + m, "gen/" + m + "/" + p + ".js", "gen/stylesheets/" + p + ".css" )
-
-    #for r in get_files( "modules/" + p ):
-     #   print r.name
-      #  if r.name.endswith( ".html" ):
-       #     exec_cmd( "cp " + r.name + " ." )
-
 
 exec_cmd( "echo > is-sim.models.js " )
 exec_cmd( "echo > is-sim.views.js " )
@@ -32,7 +26,6 @@ exec_cmd( "echo > is-sim.stylesheets.css " )
 for m in sorted(models):
     exec_cmd( "cat is-sim.models.js " + m + " > is-sim.models_tmp.js" )
     exec_cmd( "mv is-sim.models_tmp.js is-sim.models.js" )
-    #exec_cmd( "mkdir -p min; slimit -m < gen/" + o + " > min/" + o )
 for v in sorted(views):
     exec_cmd( "cat is-sim.views.js " + v + " > is-sim.views_tmp.js" )
     exec_cmd( "mv is-sim.views_tmp.js is-sim.views.js" )
@@ -40,5 +33,6 @@ for s in sorted(stylesheets):
     exec_cmd( "cat is-sim.stylesheets.css " + s + " > is-sim.stylesheets_tmp.css" )
     exec_cmd( "mv is-sim.stylesheets_tmp.css is-sim.stylesheets.css" )
 
-
-
+exec_cmd( "cp modules/Admin/admin.html ../../browser-organ" )
+exec_cmd( "cp modules/Desk/desk.html ../../browser-organ" )
+exec_cmd( "cp modules/Lab/lab.html ../../browser-organ" )
